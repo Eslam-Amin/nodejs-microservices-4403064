55aa3c60ad3386964362a8d7733dd87e3d5d7444
Eslam Amin <eslam.amin.610@gmail.com>
2025-12-01 15:52:24 +0200

    Created routes for registery

--- workspace/microservices/registry-service/routes/index.js
+++ workspace/microservices/registry-service/routes/index.js
@@ -1,19 +1,37 @@
 const express = require("express");
 
+const registery = require("../lib/Registery");
+
 const router = express.Router();
 
+function getRequestArgument(req){
+ const {serviceName, serivceVersion, servicePort} = req.params;
+  let serviceIp = req.ip;
+  if(serviceIp.includes("::1") || serviceIp.includes("::ffff:127.0.0.1")) serviceIp = "127.0.0.1";
+  return {serviceName, serivceVersion, servicePort, serviceIp}
+}
+
+
 router.get("/find/:serviceName/:serivceVersion",(req, res, next)=>{
-  return next("Not Implemented YET!")
+  const {serviceName, serivceVersion} = getRequestArgument(req);
+  const service = registery.get(serviceName, serivceVersion);
+  if(!service)
+    return res.status(404).json({error: "No Matching service found"})
+  return res.json({result: service})
 })
 
 router.put("/register/:serviceName/:serivceVersion/:servicePort",(req, res, next)=>{
-  return next("Not Implemented YET!")
+  const {serviceName, serivceVersion, servicePort, serviceIp} = getRequestArgument(req);
+  const key = registery.register(serviceName, serivceVersion,serviceIp, servicePort);
+  return res.json({result: key})
 })
 
 router.delete("/register/:serviceName/:serivceVersion/:servicePort",(req, res, next)=>{
-  return next("Not Implemented YET!")
+  const {serviceName, serivceVersion, servicePort, serviceIp} = getRequestArgument(req);
+  const key = registery.unregister(serviceName, serivceVersion,serviceIp, servicePort);
+  return res.json({result: key})
 })
-
+ 
 
 
 
